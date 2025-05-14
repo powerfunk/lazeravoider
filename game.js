8535efbf01a2727f866a843ff4e955bd55032c82
powerfunk <powerfunk@gmail.com>
2025-05-14 10:13:37 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
-import * as THREE from 'lib/three.module.js';
-import { OrbitControls } from 'lib/OrbitControls.js';
-import { io } from 'lib/socket.io.min.js';
-import * as nipplejs from 'lib/nipplejs.min.js';
+import * as THREE from './lib/three.module.js';
+import { OrbitControls } from './lib/OrbitControls.js';
+import { io } from './lib/socket.io.min.js';
+import * as nipplejs from './lib/nipplejs.min.js';
 
 class Laser {
     constructor(x, z, direction, color) {
